d339c004e47047334ce576a87ba86e8deb484acd
Alexandra <alexandra@seetickets.us>
2024-03-15 10:11:08 -0500

    added api prefix to api routes

--- server/index.js
+++ server/index.js
@@ -12,22 +12,14 @@ const AuthRoute_1 = __importDefault(require("./Routes/AuthRoute"));
 const app = (0, express_1.default)();
 require("dotenv").config();
 const { MONGO_URL, PORT } = process.env;
+//connect to MongoDB
 if (MONGO_URL) {
     mongoose_1.default
         .connect(MONGO_URL)
         .then(() => console.log("MongoDB is  connected successfully"))
         .catch((err) => console.error(err));
 }
-// Serve static files from the React app
-app.use(express_1.default.static(path_1.default.join(__dirname, "..", "build")));
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, "..", "build", "index.html"));
-});
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}!!!`);
-});
+//Middleware
 app.use((0, cors_1.default)({
     origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
@@ -35,4 +27,14 @@ app.use((0, cors_1.default)({
 }));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
-app.use("/", AuthRoute_1.default);
+// Serve static files from the React app
+app.use(express_1.default.static(path_1.default.join(__dirname, "..", "build")));
+// API routes with /api prefix
+app.use("/api", AuthRoute_1.default);
+// The "catchall" handler for any request that doesn't match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "..", "build", "index.html"));
+});
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!!!`);
+});
--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ import authRoute from "./Routes/AuthRoute";
 const app = express();
 require("dotenv").config();
 const { MONGO_URL, PORT } = process.env;
-
+//connect to MongoDB
 if (MONGO_URL) {
   mongoose
     .connect(MONGO_URL)
@@ -16,19 +16,7 @@ if (MONGO_URL) {
     .catch((err) => console.error(err));
 }
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "..", "build")));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}!!!`);
-});
-
+//Middleware
 app.use(
   cors({
     origin: ["http://localhost:3000"],
@@ -37,6 +25,19 @@ app.use(
   })
 );
 app.use(cookieParser());
-
 app.use(express.json());
-app.use("/", authRoute);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+// API routes with /api prefix
+app.use("/api", authRoute);
+
+// The "catchall" handler for any request that doesn't match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}!!!`);
+});
